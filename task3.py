43e4a3317c897cc8abc0e163aadfe6bb44b737a4
Valentin Tochilkin <foxairman@gmail.com>
2022-06-24 20:03:30 +0300

    Test telegram bot

---
+++ task3.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+
+# Объект бота
+bot = Bot(token=TELEGRAM_TOKEN)
+# Диспетчер для бота
+dp = Dispatcher(bot)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+
+# Хэндлер на команду /test1
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+if __name__ == "__main__":
+    # Запуск бота
+    executor.start_polling(dp, skip_updates=True)
