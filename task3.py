8781b82f027e885e9c126ed380e1a375d5723fd6
Valentin Tochilkin <foxairman@gmail.com>
2022-06-25 21:27:29 +0300

    Added simple FSM bot

--- task3.py
+++ task3.py
@@ -9,19 +9,24 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
-# Объект бота
-bot = Bot(token=TELEGRAM_TOKEN)
-# Диспетчер для бота
-dp = Dispatcher(bot)
+bot = Bot(token=TELEGRAM_TOKEN) # Объект бота
+dp = Dispatcher(bot) # Диспетчер для бота
+
 # Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO) 
 
+# Хэндлер на команду /test
+@dp.message_handler(commands="test")
+async def cmd_test(message: types.Message):
+    await message.reply("Test")
 
-# Хэндлер на команду /test1
-@dp.message_handler(commands="test1")
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    await message.reply("Привет! Это CrazyBot! Давай веселиться!")
 
+@dp.message_handler(commands="help")
+async def cmd_start(message: types.Message):
+    await message.reply("Привет! Это CrazyBot! Давай веселиться!")
 
 if __name__ == "__main__":
     # Запуск бота
